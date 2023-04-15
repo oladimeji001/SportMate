1d9a1369e3eeeb960c9fbc358b4cea5dc19416aa
dimeji <opeyemioladimeji.m@gmail.com>
2023-04-15 16:25:39 +0100

    SportMate

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -2,24 +2,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sportmate/components/screens/main_menu.dart';
 import 'package:sportmate/components/screens/start_menu.dart';
-class AuthGate extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthGate extends ConsumerWidget {
   const AuthGate({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    try{
+  Widget build(BuildContext context, WidgetRef ref) {
+    try {
       return StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
-            return const MainMenu();
+            return const StartMenu();
           }
 
+          return const MainMenu();
           // Render your application if authenticated
-          return const StartMenu();
         },
       );
-    } on Exception{
+    } on Exception {
       return const StartMenu();
     }
   }
